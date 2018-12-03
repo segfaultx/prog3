b2963ec13af963586bfb3be37c974805b28d4731
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-03 14:19:18 +0100

    curr bei a27 entfernt

--- Python/Blatt7/a27.py
+++ Python/Blatt7/a27.py
@@ -2,8 +2,7 @@ def dreh(lst):  # a27
     if not lst:
         return lst
     else:
-        curr = lst[-1]
-        return [curr] + dreh(lst[:-1])
+        return [lst[-1]] + dreh(lst[:-1])
 
 
 lst = [1, 2, 3, 4, 5]
