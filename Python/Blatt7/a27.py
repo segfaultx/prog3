b8e51b4646b8e7739d087fae719c1a1dfb405e3a
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-03 12:20:05 +0100

    beispielliste a27

--- Python/Blatt7/a27.py
+++ Python/Blatt7/a27.py
@@ -1,8 +1,9 @@
-def dreh(lst):
+def dreh(lst): #a27
     if not lst:
         return lst
     else:
         curr = lst[-1]
         return [curr] + dreh(lst[:-1])
-
-print(dreh([1,2,3,4,5]))
+lst=[1,2,3,4,5]
+print(lst)
+print(dreh(lst))
