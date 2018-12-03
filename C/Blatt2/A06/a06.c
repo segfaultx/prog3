f886fa83d871c3fe0204a6bd8102abe80a7e918e
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-03 14:36:41 +0100

    aufgabe 6 (C) refactored

--- C/Blatt2/A06/a06.c
+++ C/Blatt2/A06/a06.c
@@ -6,14 +6,14 @@ int main(void){
 	scanf("%lg",&input);
 	rest = (1+input)/2;
 	do{
-	new = (rest+(input/rest))/2;
+		new = (rest+(input/rest))/2;
 
-	if((rest-new)<1e-7){
-		printf("Wurzel: %-10.2f\n",new);
-		break;
-	}else{
-		rest= new;
-	}
+		if((rest-new)<1e-7){
+			printf("Wurzel: %-10.2f\n",new);
+			break;
+		}else{
+			rest= new;
+		}
 	}while(1);
 	return 0;
 }
