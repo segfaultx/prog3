ba35b20c340f61663efca7ba5c7a73c2670146ab
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-12 09:59:59 +0100

    A31 comments angepasst

--- Python/Blatt8/a31.py
+++ Python/Blatt8/a31.py
@@ -11,13 +11,14 @@ for ele in open("./{:s}".format(file), "r").readlines():
         start_dicts = True
     if start_dicts:
         ele = ele.strip().lower()  # reduce every line to lowercase, so we can properly count them
-        for letter in ele:
+        for letter in ele:  # loop to count letters, only adds alphabet letters
             if letter.isalpha():
                 alpha_res[letter] = alpha_res.get(letter, 0) + 1
-        for word in ele.split(" "):
+        for word in ele.split(" "):  # loop to add words to count dict, split by whitespaces
             if word != "":
                 res[word] = res.get(word, 0) + 1  # get value at given key, 0 default and add 1
-res = sorted(res.items(), key=lambda x: tuple(reversed(x)), reverse=True)[:25]
+res = sorted(res.items(), key=lambda x: tuple(reversed(x)), reverse=True)[
+      :25]  # sort word dict, only take first 25 entries
 alpha_res = sorted(alpha_res.items(), key=lambda x: tuple(reversed(x)), reverse=True)[:25]
 print("\nWords:\n")
 for word, amount in res:
