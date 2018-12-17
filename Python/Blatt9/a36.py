733cedb3f4ab32f4d7e53977d835cbece7de005a
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-17 15:54:28 +0100

    A36 gelöst

--- Python/Blatt9/a36.py
+++ Python/Blatt9/a36.py
@@ -1,3 +1,28 @@
 #!usr/bin/env Python3
 
+# I
 
+wohnorte_male = [name.split(";")[4].strip() for name in open("a36-bonz.txt") if name.split(";")[0] == "Herr"]
+print(sorted(wohnorte_male))
+
+# II
+
+gehalt_female = [sum(int(gehalt.split(";")[3]) for gehalt in open("a36-bonz.txt") if gehalt.startswith("Frau"))]
+print(gehalt_female)
+
+# III
+
+wohnorte_male_rich = sorted([name for name in [name.split(";") for name in open("a36-bonz.txt") if
+                                               name.split(";")[2].startswith("J")]], key=lambda x: int(x[3]),
+                            reverse=True)[:1]
+print(wohnorte_male_rich[0][4])
+
+# IV
+
+words_dict = {"Herr": 'er', "Frau": 'sie'}
+
+s = "{} {} bekommt mehr, als {} verdient!"
+hass_worte = [s.format(name[0], name[1], words_dict.get(name[0])) for name in [name.split(";") for name in
+                                                                               [name for name in open("a36-bonz.txt") if
+                                                                                int(name.split(";")[3]) > 100000]]]
+print(hass_worte)
