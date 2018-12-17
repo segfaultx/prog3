3624d938d1a8156a2f1694ef21a6d229bfd42d0b
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-17 16:19:40 +0100

    refactor a37

--- Python/Blatt9/a37.py
+++ Python/Blatt9/a37.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import glob, os
+import glob
+import os
 
 # lists 3 biggest files ending with .conf
 print(sorted([item for item in glob.glob("/etc/*.conf")], key=lambda x: os.path.getsize(x), reverse=True)[:3])
