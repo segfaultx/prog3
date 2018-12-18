6c160b1301437eecb8a8a2cd7dd881c2eb07ef8b
Amatus Muscheites <amatus.muscheites@gmail.com>
2018-12-18 08:22:20 +0100

    A35 gelöst

--- Python/Blatt9/a35.py
+++ Python/Blatt9/a35.py
@@ -2,12 +2,10 @@
 
 dec = "eniastruhdlcmogkwbzfvpjxyq"
 amount = {}
-for letter in open("a21-raeuberbrief.enc").readline().strip():
-    if letter.isalpha():
-        amount[letter] = amount.get(letter, 0) + 1
-print(amount)  # amount test
-print(dict(sorted(amount.items(), key=lambda x: (-x[1], x[0]))))  # sort test
-print(dict(zip(list(dict(sorted(amount.items(), key=lambda x: (-x[1], x[0])))), dec)))  # dec dict test
+for line in open("a21-raeuberbrief.enc"):
+    for letter in line.strip():
+        if letter.isalpha():
+            amount[letter] = amount.get(letter, 0) + 1
 out = ["".join(
     dict(zip(list(dict(sorted(amount.items(), key=lambda x: (-x[1], x[0])))), dec)).get(z, z) for
     line in open("a21-raeuberbrief.enc") for z in line.strip())]
