6010fe626e15fb439e6f94ef2b5192d3ab0bc451
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-04 12:58:57 +0100

    A35 kleinen fehler behoben

--- Python/Blatt9/a35.py
+++ Python/Blatt9/a35.py
@@ -4,7 +4,7 @@ dec = "eniastruhdlcmogkwbzfvpjxyq"
 amount = {}
 for line in open("a21-raeuberbrief.enc"):
     for letter in line.strip():
-        if letter.isalpha():
+        if letter.isalpha() and letter.islower():
             amount[letter] = amount.get(letter, 0) + 1
 out = "".join(
     dict(zip(list(dict(sorted(amount.items(), key=lambda x: (-x[1], x[0])))), dec)).get(z, z) for
