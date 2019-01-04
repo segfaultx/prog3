29703ae8ad5eae082540a0868e6e8ab217931a3d
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-04 12:55:54 +0100

    List removed from 'out' in A35

--- Python/Blatt9/a35.py
+++ Python/Blatt9/a35.py
@@ -6,8 +6,8 @@ for line in open("a21-raeuberbrief.enc"):
     for letter in line.strip():
         if letter.isalpha():
             amount[letter] = amount.get(letter, 0) + 1
-out = ["".join(
+out = "".join(
     dict(zip(list(dict(sorted(amount.items(), key=lambda x: (-x[1], x[0])))), dec)).get(z, z) for
-    line in open("a21-raeuberbrief.enc") for z in line.strip())]
+    line in open("a21-raeuberbrief.enc") for z in line.strip())
 
 print(out)
