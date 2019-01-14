cfba16ea8e6957c5e8285b75d5444d5be0458942
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-14 13:39:40 +0100

    A39 gelöst

--- Python/Blatt10/a39.py
+++ Python/Blatt10/a39.py
@@ -7,21 +7,51 @@ class Messwert:
     def __init__(self, line, value=-99.0):
         if value != -99:
             self.time = line
-            self.temp = value
+            self.temp = float(value)
         else:
             self.time = line.split(",")[0]
-            self.temp = line.split(",")[1]
+            self.temp = float(line.split(",")[1])
 
     def __repr__(self):
-        return "Messwert({},{})".format(self.time, self.temp)
+        return "Messwert('{}',{})".format(self.time, self.temp)
 
     def __eq__(self, other):
-        if self.temp != other.temp:
+        if isinstance(other, self.__class__):
+            if self.time == other.time and self.temp == other.temp:
+                return True
             return False
-        if self.time != other.time:
-            return False
-        return True
+
+    def __lt__(self, other):
+        return self.time < other.time
+
+    def __gt__(self, other):
+        return self.time > other.time
+
+    def __ge__(self, other):
+        return self.time >= other.time
+
+    def __le__(self, other):
+        return self.time <= other.time
+
+    def __key(self):
+        return self.time, self.temp
+
+    def __hash__(self):
+        return hash(self.__key())
 
 
-mw = Messwert("2019-01-11 17:45:01.356640,19.5")
-print(eval(repr(mw)) == mw)
+if __name__ == "__main__":
+    mw = Messwert("2019-01-11 17:45:01.356640,19.5")  # 2
+    mw2 = Messwert("2019-01-12 17:45:01.356640,19.5")  # 3
+    mw3 = Messwert("2019-01-12 17:49:01.356640,19.5")  # 4
+    mw4 = Messwert("2019-01-13 17:45:01.356640,19.5")  # 5
+    mw5 = Messwert("2019-01-09 17:45:01.356640,19.5")  # 1
+    mw6 = Messwert("2019-01-15 17:45:01.356640,19.5")  # 6
+    mw7 = Messwert("2019-01-16 17:45:01.356640,19.5")  # 7
+    print(mw)
+    print(eval(repr(mw)) == mw)
+    lst = [mw7, mw, mw5, mw2, mw4, mw3, mw6]
+    print(lst)
+    print(sorted(lst))
+
+    print(set(lst))
