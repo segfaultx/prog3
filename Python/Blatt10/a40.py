ef67abe147ce4de71ceaac0735ed5e06dcd5652a
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-14 13:50:43 +0100

    A40 begonnen

--- Python/Blatt10/a40.py
+++ Python/Blatt10/a40.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
 
+from a39 import Messwert
+
+
+class Messreihe:
+    """Class to hold multiple tempvalues with timestamps"""
+
+    def __init__(self, data=None):
+        self._values = []
+        if data:
+            for item in data:
+                self._values.append(Messwert(item))
+
+    def __len__(self):
+        return len(self._values)
+
+
+if __name__ == "__main__":
+    mr = Messreihe()
+    mr2 = Messreihe(open("messwerte.csv"))
+    print(mr, mr2, len(mr2))
