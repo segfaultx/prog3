cbe884ecd86f27b2a2251a629f0156dd8cad1f43
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-16 07:53:50 +0100

    if name==main A41 nachgetragen

--- Python/Blatt10/a41.py
+++ Python/Blatt10/a41.py
@@ -2,11 +2,12 @@
 
 from a40 import Messreihe
 
-mr = Messreihe(open("messwerte.csv"))
-print("Länge", len(mr))
-print("Max", max([val.temp for val in mr]))
-print("Min", min([val.temp for val in mr]))
-print("Temp>33°", [mw for mw in mr if mw.temp > 33])
-print(len([mw for mw in mr["2017"] if mw.temp > 26]))
-print([mw for mw in mr if int(mw.temp) == 17][-1])
-print(sum(mw.temp for mw in [mw for mw in mr["2017-1"]])/len(mr["2017-1"]))
+if __name__ == "__main__":
+    mr = Messreihe(open("messwerte.csv"))
+    print("Länge", len(mr))
+    print("Max", max([val.temp for val in mr]))
+    print("Min", min([val.temp for val in mr]))
+    print("Temp>33°", [mw for mw in mr if mw.temp > 33])
+    print(len([mw for mw in mr["2017"] if mw.temp > 26]))
+    print([mw for mw in mr if int(mw.temp) == 17][-1])
+    print(sum(mw.temp for mw in [mw for mw in mr["2017-1"]]) / len(mr["2017-1"]))
