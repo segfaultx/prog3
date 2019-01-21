fb6be186e7d85a9d41a79dcdeb1423306d3ca76f
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-21 15:15:02 +0100

    python aufgabe schöner gelöst

--- Onlinetest_Ueben/Python/a2.py
+++ Onlinetest_Ueben/Python/a2.py
@@ -11,7 +11,15 @@ def statistik(filename):
             customers_dict[line[0]] = customers_dict.get(line[0], {})
             customers_dict[line[0]][line[1]] = customers_dict[line[0]].get(line[0], int(line[2]))
     for key in sorted(customers_dict):
-        print("{}:".format(key), end='')
-        for k in sorted(customers_dict[key].keys()):
-            print(" {}({})".format(k, customers_dict[key][k]), end='')
-        print("")
+        print(f'{key}'," ".join([f'{k}({v})' for k,v in sorted(customers_dict[key].items())]))
+
+
+if __name__ == "__main__":
+    print("Test 1")
+    statistik("bestellungen-1.txt")
+    print("Test 2")
+    statistik("bestellungen-2.txt")
+    print("Test 3")
+    statistik("bestellungen-3.txt")
+    print("Test 4")
+    statistik("bestellungen-4.txt")
