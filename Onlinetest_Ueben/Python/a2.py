1182bcf968ed2a3b4184fa5ed28f691b288a283a
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-21 14:51:38 +0100

    Python aufgabe gelöst

--- Onlinetest_Ueben/Python/a2.py
+++ Onlinetest_Ueben/Python/a2.py
@@ -5,12 +5,13 @@ def statistik(filename):
     customers_dict = {}
     for line in open(filename):
         line = line.split(";")
-        if line[1] in customers_dict[line[0]]:
-            customers_dict.get(line[0])
+        if line[0] in customers_dict.keys():
+            customers_dict[line[0]][line[1]] = customers_dict[line[0]].get(line[1], 0) + int(line[2])
         else:
-            customers_dict[line[0]] = customers_dict.get(line[0], []).append((line[1], line[2]))
-
-
-if __name__ == "__main__":
-    # statistik("bestellungen-1.txt")
-    print("Hallo" in ("Hallo", 1))
+            customers_dict[line[0]] = customers_dict.get(line[0], {})
+            customers_dict[line[0]][line[1]] = customers_dict[line[0]].get(line[0], int(line[2]))
+    for key in sorted(customers_dict):
+        print("{}:".format(key), end='')
+        for k in sorted(customers_dict[key].keys()):
+            print(" {}({})".format(k, customers_dict[key][k]), end='')
+        print("")
