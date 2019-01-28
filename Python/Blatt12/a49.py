8385ccf8f069aec603ba87935c203997e0707dbd
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-28 14:37:29 +0100

    A49 zu A50 refactored, A50 gelöst

--- Python/Blatt12/a49.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import re
-import sys
-
-for line in sys.stdin:
-    print(re.sub(r'\w+', lambda x: "".join(reversed(list(x.group())) if x.group().isalpha() else x.group()), line))
---
+++ Python/Blatt12/a50.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+for line in sys.stdin:
+    print(re.sub(r'[a-zA-Z]+', lambda x: "[" + "".join(reversed(list(x.group())) if x.group().isalpha() else x.group()) + "]",
+                 line))
