7faf69468deedd4fffb8fcc831eb99d045a13b02
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-28 18:09:39 +0100

    regex angepasst a49

--- Python/Blatt12/a49.py
+++ Python/Blatt12/a49.py
@@ -5,10 +5,12 @@ import re
 
 amount_dict = {}
 exp1 = re.compile(
-    r'(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*(?P<time>\d{2}:\d{2}:\d{2}).*(?P<get>GET (/.+)+\.(png|gif|jpg))')
+    r'(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*(?P<time>\d{2}:\d{2}:\d{2}).*(?P<get>GET (/.+?)(\.(?P<extension>png|gif|jpg))?) HTTP')
 for line in open(sys.argv[1]):
     filetype = exp1.match(line)
     if filetype:
         print(filetype.groupdict())
 
 print(amount_dict)
+
+wip = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[ -]+\[[^:]+:(\d{2}:\d{2}:\d{2})[^\]]*\]\s"(GET (/.+?)(\.(png|gif|jpg))?) HTTP'
