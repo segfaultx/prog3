0401d314074705eaeaa11848df18e3e3028c40bb
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-01-28 14:39:14 +0100

    ausgabe a50 angepasst

--- Python/Blatt12/a50.py
+++ Python/Blatt12/a50.py
@@ -4,5 +4,5 @@ import re
 import sys
 
 for line in sys.stdin:
-    print(re.sub(r'[a-zA-Z]+', lambda x: "[" + "".join(reversed(list(x.group())) if x.group().isalpha() else x.group()) + "]",
-                 line))
+    print(
+        re.sub(r'[a-zA-Z]+', lambda x: "".join(reversed(list(x.group())) if x.group().isalpha() else x.group()), line))
