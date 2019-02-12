6eb9a2bc639720d8a1411d5f43eec6d62bdf3cd2
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-02-12 16:26:39 +0100

    netter lösung

--- Private_Uebung/Onlinetest1112_C/a1.c
+++ Private_Uebung/Onlinetest1112_C/a1.c
@@ -1,35 +1,5 @@
 #include <stdio.h>
 
-
-int str_del_lowervowels(char *s){
-	char *lowerVowels ="aeiou";
-	char *v;
-	char *res, *cpyhlpr;
-	int amount = 0;
-	res = s;
-	while(*s){
-		v = lowerVowels;
-		while(*v){
-			if(*s == *v){
-				res++;
-				cpyhlpr = s;
-				amount+=1;
-				do{
-					*cpyhlpr++=*res++;
-				}while(*res);
-				*cpyhlpr='\0';
-				res = s;
-				v = lowerVowels;
-				continue;
-			}
-			v++;
-		}
-		s++;
-		res++;
-	}
-	return amount;
-}
-
 int checklowerVowel(char n){
 	switch(n){
 		case 'a':
@@ -40,6 +10,23 @@ int checklowerVowel(char n){
 	}
 	return 0;
 }
+
+int str_del_lowervowels(char *s){
+	char *res;
+	int amount = 0;
+	res = s;
+	while(*s){
+		if(!checklowerVowel(*res)){
+			*s++=*res++;
+		}else{
+			amount+=1;
+			res++;
+		}
+	}
+	return amount;
+}
+
+
 
 
 int main(int argc, char **argv){
