dbc1b7e9fb0aa947e1a10eec78aa26a900465098
Amatus Muscheites <amatus.muscheites@gmail.com>
2019-02-15 11:44:35 +0100

    File einlesen übung, C

---
+++ Private_Uebung/File_Einlsen/a1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	FILE *f;
+	char satz[100];
+	f = fopen("saetze.txt","r");
+	while(fgets(satz,100,f) != NULL){
+		printf("%s\n",satz);
+	}
+	return 0;
+}
